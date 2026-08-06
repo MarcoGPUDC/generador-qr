62f0f630565fc0911880ec88d31705aa5bdedaa7
Marco <135654717+MarcoGPUDC@users.noreply.github.com>
2026-08-06 11:37:46 -0300

    ORM

---
+++ modules/login.js
@@ -0,0 +1,92 @@
+
+
+document.getElementById("signup-btn").addEventListener("click", async () => {
+    document.getElementById("singupContainer").style.display = "block";
+})
+
+const signupSubmitBtn = document.getElementById("signup-submit-btn");
+signupSubmitBtn.addEventListener("click", async () => {
+    const username = document.getElementById("signup-username").value;
+    const password = document.getElementById("signup-password").value;
+    const email = document.getElementById("signup-email").value;
+    const { data, error } = await bbdd.auth.signUp({
+        email: email,
+        password: password, // Pasas la clave cruda. Supabase la hashea en su servidor.
+        display: username
+    })
+    if (error) {
+        console.error(error);
+        alert("Error al registrarse: " + error.message);
+    } else {
+        alert("Usuario registrado correctamente");
+        document.getElementById("singupContainer").style.display = "none";
+        document.getElementById("signup-username").value = "";
+        document.getElementById("signup-password").value = "";
+        document.getElementById("signup-email").value = "";
+    }
+});
+
+const loginBtn = document.getElementById("login-btn");
+loginBtn.addEventListener("click", async () => {
+    const passwordInput = document.getElementById("password");
+    const usernameInput = document.getElementById("username");
+    const { data, error } = await bbdd.auth.signInWithPassword({
+        email: usernameInput.value,
+        password: passwordInput.value, // Supabase comprueba el hash internamente
+    })
+    if (!error) {
+        alert("Usuario logueado correctamente");
+        const btnLogout = document.getElementById("logout-btn");
+        const btnSingup = document.getElementById("signup-btn");
+        const btnSaveQr = document.getElementById("save-qr-btn");
+        btnSingup.style.display = "none";
+        btnLogout.style.display = "inline-block";
+        btnSaveQr.style.display = "inline-block";
+        loginBtn.style.display = "none";
+        usernameInput.style.display = "none";
+        passwordInput.style.display = "none";
+        
+        mostrarGenerados(await userAllCodes(data.user.id));
+        passwordInput.value = "";
+        const btnGuardarQr = document.getElementById("save-qr-btn");
+        btnGuardarQr.addEventListener("click", async () => {
+            await guardarQr(data.user.id);
+            mostrarGenerados(await userAllCodes(data.user.id));
+        });
+        const btnEliminar = document.getElementById("btn-eliminar");
+        btnEliminar.addEventListener("click", async () => {
+            await eliminarQr(btnEliminar.getAttribute("data-id"));
+            mostrarGenerados(await userAllCodes(data.user.id));
+        });
+    } else {
+        console.log(error);
+        alert("Usuario o contraseña incorrectos");
+    }
+});
+
+const logoutBtn = document.getElementById("logout-btn");
+logoutBtn.addEventListener("click", async () => {
+    const { error } = await bbdd.auth.signOut()
+
+    if (error) {
+        console.error("Error al cerrar sesión:", error.message)
+    } else {
+        console.log("Sesión cerrada correctamente")
+    }
+    const btnSingup = document.getElementById("signup-btn");
+    const btnLogout = document.getElementById("logout-btn");
+    const loginBtn = document.getElementById("login-btn");
+    const btnSaveQr = document.getElementById("save-qr-btn");
+    const usernameInput = document.getElementById("username");
+    const passwordInput = document.getElementById("password");
+    const generadosContainer = document.getElementById("generados-container");
+    
+    generadosContainer.style.display = "none";
+    generadosContainer.innerHTML = "";
+    btnSingup.style.display = "inline-block";
+    btnLogout.style.display = "none";
+    loginBtn.style.display = "inline-block";
+    usernameInput.style.display = "inline-block";
+    passwordInput.style.display = "inline-block";
+    btnSaveQr.style.display = "none";
+});
--- scripts.js
+++ scripts.js
@@ -1,19 +1,132 @@
-
 const btn = document.getElementById("generar-btn");
-const qrContainer = document.getElementById("qrcode");
+const qrContainer = document.getElementById("qrcode"); 
 
+const bbdd = window.supabase.createClient(
+    "https://bfxsiqkicxetuwrygtij.supabase.co",
+    "sb_publishable_LXqdCSrppPPfRVTX0EmdbQ_TNjOhmNh"
+);
+
+const params = new URLSearchParams(window.location.search);
+const codigo = params.get("qr");
+//const codigo = window.location.pathname.substring(1);
+
+async function redirigir() {
+    const { data, error } = await bbdd
+        .from("generados")
+        .select("url")
+        .eq("uri", codigo)
+        .single();
+
+    if (data) {
+        window.location.replace(data.url);
+    }
+}
+
+if (codigo) {
+    redirigir();
+}
+
+async function userAllCodes(id) {
+    const { data, error } = await bbdd
+    .from("generados")
+    .select("*")
+    .eq("user_id", id);
+    if (error) {
+        console.error(error);
+    } else {
+        return data;
+    }
+}
+
+function generarQr(uri) {
+    qrContainer.innerHTML = "";
+    const url = window.location.origin + "/" + uri;
+    new QRCode(qrContainer, {
+        text: url,
+        width: 512,
+        height: 512
+    });
+
+}
 btn.addEventListener("click", () => {
 
     const url = document
-    .getElementById("url-input")
-    .value;
+        .getElementById("url-input")
+        .value;
 
-    qrContainer.innerHTML = "";
-
-    new QRCode(qrContainer, {
-    text: url,
-    width: 512,
-    height: 512
-    });
+    generarQr(url);
 
 });
+
+function mostrarGenerados(data) {
+    const generadosContainer = document.getElementById("generados-container");
+    generadosContainer.innerHTML = "";
+    const list = document.createElement("ul");
+    data.forEach(item => {
+        const listItem = document.createElement("li");
+        listItem.textContent = `URL: ${item.url}, URI: ${item.uri}`;
+        listItem.innerHTML += `<button id="btn-verqr" onclick="generarQr('${item.uri}')">Ver QR</button> <button id="btn-editar" onclick="editarQr('${item.id}')">Editar</button> <button id="btn-eliminar" data-id="${item.id}">X</button>`;
+        list.appendChild(listItem);
+    });
+    generadosContainer.appendChild(list);
+    generadosContainer.style.display = "block";
+}
+
+async function editarQr(id) {
+    const { data, error } = await bbdd
+        .from("generados")
+        .select("*")
+        .eq("id", id)
+        .single();
+    if (data) {
+        const newUrl = prompt("Ingrese la nueva URL:", data.url);
+        if (newUrl) {
+            const { error } = await bbdd
+                .from("generados")
+                .update({ url: newUrl })
+                .eq("id", id);
+            if (error) {
+                console.error(error);
+            } else {
+                mostrarGenerados(await userAllCodes(data.user_id));
+                alert("URL actualizada correctamente");
+            }
+
+        }
+    } else {
+        console.error(error);
+    }
+};
+
+async function guardarQr(id) {
+    const { data, error } = await bbdd
+        .from("generados")
+        .insert([
+            { 
+                uri: Math.random().toString(36).substring(2, 10), 
+                user_id: id,
+                url: document.getElementById("url-input").value
+            }
+            ])
+        .select()
+    if (error) {
+        console.error(error);
+    }else {
+        mostrarGenerados(await userAllCodes(id));
+        alert("QR guardado correctamente");
+    }
+}
+
+async function eliminarQr(id) {
+    const { data, error } = await bbdd
+        .from("generados")
+        .delete()
+        .eq("id", id);
+    if (error) {
+        console.error(error);
+    } else {
+        alert("QR eliminado correctamente");
+    }
+}
+
+const botonGuardarQr = document.getElementById("save-qr-btn");
